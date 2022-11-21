1b6739e86fee4c6d3ca743692ca90ecffc83f1a0
Youri <youridibbet@gmail.com>
2022-11-21 16:37:07 +0100

    Updated 'Contact Us'

--- pages/05_Contact Us.py
+++ pages/05_Contact Us.py
@@ -3,14 +3,19 @@ from sendmail import send_email
 
 st.header("Contact Us")
 st.write("Voor vragen zijn wij via het onderstaande formulier te bereiken")
+keuze_menu = "Website", "Feedback", "Overig"
 
 with st.form(key="email_form", clear_on_submit=True):
-    user_email = st.text_input('', placeholder='Vul je hier je emailadres in')
-    raw_message = st.text_area("", placeholder='Vul hier je vraag in')
+    user_email = st.text_input("email", placeholder='Vul je hier je emailadres in', label_visibility='collapsed')
+    topic = st.selectbox("Waar gaat je vraag over?", options=(keuze_menu))
+    raw_message = st.text_area("message", placeholder='Vul hier je vraag in', label_visibility='hidden')
     message = f'''\
 Subject: New email from {user_email}
+
+Onderwerp: {topic}
 {raw_message}
 '''
+
     user_send = st.form_submit_button()
     if user_send:
         send_email(message)
